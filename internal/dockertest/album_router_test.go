144174c12b5e98e0a2cebfc7d5a620e70b7d3135
Florent Legname <floleg@lunagri.net>
2022-10-02 16:06:30 +0200

    refactor album test

--- internal/dockertest/album_router_test.go
+++ internal/dockertest/album_router_test.go
@@ -51,50 +51,64 @@ func TestFindAll(t *testing.T) {
 	}
 }
 
-// Passing a string as ID should return 400
-func TestAlbumByIdWithString(t *testing.T) {
-	router := router.NewRouter()
-
-	w := httptest.NewRecorder()
-
-	req, _ := http.NewRequest("GET", "/album/misguided-id", nil)
-	router.ServeHTTP(w, req)
-
-	if w.Code != 400 {
-		t.Errorf("got %v, want %v", w.Code, 400)
-	}
-
-	if w.Body.String() != "{\"message\":\"bad request\"}" {
-		t.Errorf("got %v, want %v", w.Body.String(), "{\"message\":\"bad request\"}")
-	}
-}
-
+// Two cases: integer parameter and string parameter
 func TestAlbumById(t *testing.T) {
+	var tests = []struct {
+		name  string
+		panic bool
+	}{
+		{name: "int"},
+		{name: "string"},
+	}
+
 	router := router.NewRouter()
 
-	w := httptest.NewRecorder()
+	for _, tt := range tests {
+		testname := fmt.Sprintf("%s", tt.name)
+		t.Run(testname, func(t *testing.T) {
+			if tt.name == "int" {
+				// First search in album list to retreieve an actual album id
+				findAllReq, _ := http.NewRequest("GET", "/album?offset=0&limit=1", nil)
 
-	// First search in album list to retreieve an actual album id
-	findAllReq, _ := http.NewRequest("GET", "/album?offset=0&limit=1", nil)
-	router.ServeHTTP(w, findAllReq)
-	albums := []model.Album{}
-	json.NewDecoder(w.Body).Decode(&albums)
+				w := httptest.NewRecorder()
 
-	// Fetch single album with previously retrieved id
-	req, _ := http.NewRequest("GET", fmt.Sprintf("/album/%v", albums[0].ID), nil)
-	router.ServeHTTP(w, req)
+				router.ServeHTTP(w, findAllReq)
+				albums := []model.Album{}
+				json.NewDecoder(w.Body).Decode(&albums)
 
-	album := model.Album{}
+				// Fetch single album with previously retrieved id
+				req, _ := http.NewRequest("GET", fmt.Sprintf("/album/%v", albums[0].ID), nil)
+				router.ServeHTTP(w, req)
 
-	if w.Code != 200 {
-		t.Errorf("got %v, want %v", w.Code, 200)
+				album := model.Album{}
+
+				if w.Code != 200 {
+					t.Errorf("got %v, want %v", w.Code, 200)
+				}
+
+				json.NewDecoder(w.Body).Decode(&album)
+
+				if len(album.Tracks) != 10 {
+					t.Errorf("got %v, want %v", len(album.Tracks), 10)
+				}
+			} else {
+				req, _ := http.NewRequest("GET", "/album/misguided-id", nil)
+
+				w := httptest.NewRecorder()
+
+				router.ServeHTTP(w, req)
+
+				if w.Code != 400 {
+					t.Errorf("got %v, want %v", w.Code, 400)
+				}
+
+				if w.Body.String() != "{\"message\":\"bad request\"}" {
+					t.Errorf("got %v, want %v", w.Body.String(), "{\"message\":\"bad request\"}")
+				}
+			}
+		})
 	}
 
-	json.NewDecoder(w.Body).Decode(&album)
-
-	if len(album.Tracks) != 10 {
-		t.Errorf("got %v, want %v", len(album.Tracks), 10)
-	}
 }
 
 func TestSearch(t *testing.T) {
