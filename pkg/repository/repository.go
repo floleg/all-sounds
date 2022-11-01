de290045bd2cb7d1b0b75a103602b1f0422183e6
Florent Legname <floleg@lunagri.net>
2022-11-01 13:07:25 +0100

    comments on  repository base

--- pkg/repository/repository.go
+++ pkg/repository/repository.go
@@ -12,12 +12,23 @@ type BaseRepository interface {
 
 type Repository struct{}
 
+// FindAll performs a select query and returns an interface of the given data parameter, filtered with:
+//   - offset
+//   - limit
+//
+// The results will be ordered based on the column name passed in the order parameter.
 func (b Repository) FindAll(offset int, limit int, data interface{}, order string) interface{} {
 	db.DBCon.Order(fmt.Sprintf("%s asc", order)).Limit(limit).Offset(offset).Find(&data)
 
 	return data
 }
 
+// Search performs a select query and returns an interface of the given data parameter, filtered with:
+//   - offset
+//   - limit
+//   - query: where clause
+//
+// The results will be ordered based on the column name passed in the order parameter.
 func (b Repository) Search(offset int, limit int, query string, data interface{}, order string) interface{} {
 	db.DBCon.Order(fmt.Sprintf("%s asc", order)).Limit(limit).Offset(offset).
 		Where(fmt.Sprintf("%s LIKE ?", order), "%"+query+"%").Find(&data)
@@ -25,6 +36,7 @@ func (b Repository) Search(offset int, limit int, query string, data interface{}
 	return data
 }
 
+// FindById retrieves an interface by id, based on the implementation of the input data parameter
 func (b Repository) FindById(id int, data interface{}) *interface{} {
 	db.DBCon.First(&data, id)
 
